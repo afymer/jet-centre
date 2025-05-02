a1ead06735d54929c67d44e4a4fccf0d21f7e866
Remy Fayet <remy.fayet@telecom-paris.fr>
2025-05-02 09:01:02 +0200

    (dev) add: DraggableFloatingWindow component

---
+++ src/components/dev/draggable-floating-window.tsx
@@ -0,0 +1,136 @@
+'use client';
+
+import { PointerEventHandler, ReactNode, useEffect, useRef, useState } from 'react';
+
+import { Button } from '../ui/button';
+
+interface WindowPosition {
+    x: number;
+    y: number;
+}
+
+interface WindowSize {
+    width: number;
+    height: number;
+}
+
+interface WindowState {
+    expand: 'expanded' | 'collapsed';
+}
+
+export default function DraggableFloatingWindow({ children }: { children?: ReactNode }) {
+    const [windowPosition, setWindowPosition] = useState<WindowPosition>({
+        x: (window?.innerWidth || 500) - 500,
+        y: (window?.innerHeight || 500) - 500,
+    });
+    const [windowSize, setWindowSize] = useState<WindowSize>({ width: 500, height: 500 });
+    const [drag, setDrag] = useState<boolean>(false);
+    const [resize, setResize] = useState<boolean>(false);
+    const [dragOffset, setDragOffset] = useState<WindowPosition>({ x: 0, y: 0 });
+    const [windowState, setWindowState] = useState<WindowState>({ expand: 'expanded' });
+    const resizeRef = useRef<HTMLDivElement>(null);
+
+    const toggleExpandState = () => {
+        if (windowState.expand == 'collapsed') {
+            setWindowState({ ...windowState, expand: 'expanded' });
+        } else {
+            setWindowState({ ...windowState, expand: 'collapsed' });
+        }
+    };
+
+    const handlePointerDownMove: PointerEventHandler<HTMLDivElement> = (event) => {
+        setDrag(true);
+        setDragOffset({
+            x: event.clientX - windowPosition.x,
+            y: event.clientY - windowPosition.y,
+        });
+    };
+
+    const handlePointerDownResize: PointerEventHandler<HTMLDivElement> = (event) => {
+        setResize(true);
+        // const rect = resizeRef.current?.getBoundingClientRect();
+        setDragOffset({
+            x: event.clientX - windowSize.width,
+            y: event.clientY - windowSize.height,
+        });
+    };
+
+    useEffect(() => {
+        const handlePointerUp = () => {
+            setDrag(false);
+            setResize(false);
+        };
+
+        const handlePointerMove = (event: PointerEvent) => {
+            if (drag) {
+                setWindowPosition({
+                    x: event.clientX - dragOffset.x,
+                    y: event.clientY - dragOffset.y,
+                });
+            } else if (resize) {
+                setWindowSize({
+                    width: event.clientX - dragOffset.x,
+                    height: event.clientY - dragOffset.y,
+                });
+            }
+        };
+
+        document.addEventListener('pointerup', handlePointerUp);
+        document.addEventListener('pointermove', handlePointerMove);
+
+        return () => {
+            document.removeEventListener('pointerup', handlePointerUp);
+            document.removeEventListener('pointermove', handlePointerMove);
+        };
+    });
+
+    return (
+        <>
+            {windowState.expand == 'expanded' && (
+                <div
+                    className="absolute rounded z-20 flex flex-col overflow-hidden"
+                    style={{
+                        left: windowPosition.x + 'px',
+                        top: windowPosition.y + 'px',
+                        width: windowSize.width + 'px',
+                        height: windowSize.height + 'px',
+                    }}
+                >
+                    <div
+                        className="min-h-2 bg-box-title select-none flex content-between place-items-center"
+                        onPointerDown={handlePointerDownMove}
+                    >
+                        <div>
+                            <Button variant="ghost" onClick={toggleExpandState}>
+                                -
+                            </Button>
+                        </div>
+                        <div className="pl-4 pr-4">Window Title</div>
+                    </div>
+                    <div className="p-4 bg-box-background h-full">
+                        {children}
+                        <div
+                            className="absolute size-4 cursor-se-resize bg-red-400 bottom-0 right-0"
+                            onPointerDown={handlePointerDownResize}
+                            ref={resizeRef}
+                        ></div>
+                    </div>
+                </div>
+            )}
+            {windowState.expand == 'collapsed' && (
+                <div
+                    className="absolute rounded-full z-20 flex flex-col overflow-hidden bg-box-title"
+                    style={{ left: windowPosition.x + 'px', top: windowPosition.y + 'px' }}
+                    onPointerDown={handlePointerDownMove}
+                    onClick={() => {
+                        if (!drag) toggleExpandState();
+                    }}
+                >
+                    <Button variant="ghost" onClick={toggleExpandState}>
+                        +
+                    </Button>
+                </div>
+            )}
+        </>
+    );
+}
